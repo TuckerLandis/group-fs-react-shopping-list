4272129882f7f039ef347b8e8b475d537c842d98
TuckerLandis <landistuckerc@gmail.com>
2021-06-04 10:03:58 -0500

    put backend

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -6,4 +6,21 @@ const pool = require('../modules/pool.js');
 
 
 
+
+//PUT 
+router.put('/:id', (req, res) => {
+    console.log('got to PUT');
+    console.log('purchasing: # ', req.params.id);
+    
+    const queryText = `UPDATE "list" SET "purchased"='true' WHERE "list".id = $1 ; ` ;
+
+    pool.query(queryText, [req.params.id])
+    .then(result => {
+        res.sendStatus(200)
+    }).catch(error => {
+        console.log('error updating ', error);
+        res.sendStatus(500)
+    })
+})
+
 module.exports = router;
