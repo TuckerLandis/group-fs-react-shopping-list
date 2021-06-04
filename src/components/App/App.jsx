54fcf2e44ff96eca72f90e53b3ebea039c8d1046
TuckerLandis <landistuckerc@gmail.com>
2021-06-04 10:15:07 -0500

    put frontend

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 
-import Header from '../Header/Header.jsx'
+import Header from '../Header/Header.jsx';
 import './App.css';
 
 
 function App() {
+
+    const purchaseItem = () => {
+
+        Axios.put(`/list/${itemId}`)
+        .then(response => {
+            //get list
+        }).catch(error => {
+            console.log('error purchasing item ', error);
+        })
+    } 
+
+
     return (
         <div className="App">
             <Header />
