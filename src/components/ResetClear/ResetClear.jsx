4b8971a43353593b253c6e19d7e23a0adcdfff03
Mary Mettille <mary.mettille.brist@gmail.com>
2021-06-04 14:36:17 -0500

    Confirmation Works

--- src/components/ResetClear/ResetClear.jsx
+++ src/components/ResetClear/ResetClear.jsx
@@ -3,11 +3,25 @@ import swal from 'sweetalert';
 function ResetClear ({shoppingList, removeItem, resetPurchased}) {
     // function for the reset button event listener
     const handleReset = () => {
-        console.log('Clicked!');
-        // iterate over each item in list and make a delete request for each one individually
-        
-        shoppingList.forEach(item => resetPurchased(item.id));
-    }
+        swal({
+            title: "Are You Sure?",
+            text: "This will mark all your items as not purchased. This cannot be undone",
+            icon: "warning",
+            dangerMode: true,
+            buttons: ["Cancel", "Yes, I am sure."]
+        }).then( (willDelete) => {
+            if (willDelete) {
+                swal("Your list has been reset!",{
+                    icon: "success",
+                })
+                // iterate over each item in list and make a delete request for each one individually
+                shoppingList.forEach(item => resetPurchased(item.id))
+        } else {
+            swal("Your list is safe!");
+        }
+    })
+              
+}
     // function for the clear button event listener
     const handleClear = () => {
         console.log('Clicked!');
