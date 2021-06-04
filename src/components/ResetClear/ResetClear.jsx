1c2289e25e4aa713cee285f3d17e94eea5c4b368
Mary Mettille <mary.mettille.brist@gmail.com>
2021-06-04 14:27:15 -0500

    Confrimation sweet alert

--- src/components/ResetClear/ResetClear.jsx
+++ src/components/ResetClear/ResetClear.jsx
@@ -1,18 +1,22 @@
-import SweetAlert from 'sweetalert2-react';
+import swal from 'sweetalert';
 
 function ResetClear ({shoppingList, removeItem, resetPurchased}) {
     // function for the reset button event listener
     const handleReset = () => {
-        sweetAlert({
+        swal({
             title: "Are You Sure?",
             text: "This will mark all your items as not purchased. This cannot be undone",
-            icon: warning,
+            icon: "warning",
             dangerMode: true,
-        })
-        console.log('Clicked!');
-        // iterate over each item in list and make a delete request for each one individually
-        shoppingList.forEach(item => resetPurchased(item.id));
-    }
+            buttons: ["Cancel", "Yes, I am sure."]
+        }).then( result => {
+            if (result.value) {
+                // iterate over each item in list and make a delete request for each one individually
+                shoppingList.forEach(item => resetPurchased(item.id))
+        }
+    })
+              
+}
     // function for the clear button event listener
     const handleClear = () => {
         console.log('Clicked!');
