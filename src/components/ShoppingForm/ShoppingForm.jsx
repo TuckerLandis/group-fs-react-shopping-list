98242e4a49b6b0713740a6157fb3746ba1185c03
TuckerLandis <landistuckerc@gmail.com>
2021-06-04 12:53:36 -0500

    horizontal spacing

--- src/components/ShoppingForm/ShoppingForm.jsx
+++ src/components/ShoppingForm/ShoppingForm.jsx
@@ -1,6 +1,7 @@
 import {useState} from 'react';
 import axios from 'axios';
 
+import './ShoppingForm.css'
 function ShoppingForm({addToList}){
 
     const [shoppingItem, setShoppingItem] = useState('');
