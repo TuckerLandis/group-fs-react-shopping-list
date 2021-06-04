dc07d6810fd19bcacf9a356e509216cc50f1ab88
Emry Brisky <eabrisky@gmail.com>
2021-06-04 11:44:32 -0500

    import added

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 import Header from '../Header/Header.jsx';
 import './App.css';
 import ShoppingForm from '../ShoppingForm/ShoppingForm.jsx';
+import ShoppingList from '../ShoppingList/ShoppingList.jsx';
 
 
 function App() {
