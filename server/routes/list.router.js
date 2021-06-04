eb8d9964f2d3f6d1b86a9bbb6b39370e0e851ba8
Mary Mettille <mary.mettille.brist@gmail.com>
2021-06-04 10:14:36 -0500

    Server router to put request in list.router.js

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -4,6 +4,22 @@ const pool = require('../modules/pool.js');
 
 // TODO - Add routes here...
 
-
+// ⬇ Post request for adding a new item
+router.post('/', (req, res) => {
+    // ⬇ Sets up what we want from the user
+    const item = req.body;
+    // ⬇ Tells sql WHAT and WHERE we'd like to do AND sanitizes 
+    const queryText = `INSERT INTO list ("name", "quantity", "unit") VALUES ($1, $2, $3)`;
+    // ⬇ The $1, $2 will get substituted with the values from the array
+    pool.query(queryText, [item.name, item.quantity, item.unit])
+        .then( result => {
+            console.log(`Added item to the database`, item);
+            res.sendStatus(201);
+        })
+        .catch( err => {
+            console.log(`Error adding ${queryText} to the database`, err);
+            res.sendStatus(500);
+        })
+})
 
 module.exports = router;
