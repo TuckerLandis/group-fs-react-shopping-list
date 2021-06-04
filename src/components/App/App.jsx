0584a3d8493c9f8db4089de3bc01073e1c5ea877
Mary Mettille <mary.mettille.brist@gmail.com>
2021-06-04 10:04:11 -0500

    Front Side to put request in app.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,17 @@ import React from 'react';
 import Header from '../Header/Header.jsx'
 import './App.css';
 
+// ⬇ Function to add a new item to the list
+const addToList = (newFoodItem) => {
+    axios.post('/list', {newFoodItem})
+    .then( response => {
+    // ⬇ Calls function to get the updated shopping list from the database
+    getShoppingList();
+    })
+    .catch( err => {
+        console.log('error adding item', err)
+    })
+}
 
 function App() {
     return (
